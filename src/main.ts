90d881d8fbc1b01cdde535cc7d335e242ec1b4ea
Kumquattt <70588763+Kumquattt@users.noreply.github.com>
2025-12-29 18:15:59 +0100

    Dependency cleanup & put game on root path

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ const router = createRouter({
   // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   history: createWebHashHistory(),
   routes: [
-    { path: '/', component: MainMenuVue },
+    { path: '/', component: GamePageVue },
     { path: '/menu', component: MainMenuVue },
     { path: '/game', component: GamePageVue },
     { path: '/test', component: TestFileVue }
